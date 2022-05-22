e0ac1171ec63fccd747acf4b04289020426d718b
Kevin Batdorf <kbat82@gmail.com>
2022-05-22 17:55:17 -0400

    wip

--- scripts/move-db.ts
+++ scripts/move-db.ts
@@ -2,7 +2,7 @@ import { writeFile, readFile } from 'fs'
 import { join } from 'path'
 
 // move db file
-readFile(join(__dirname, 'games.db'), (err, data) => {
+readFile(join(__dirname, '../prisma/games.db'), (err, data) => {
     if (err) throw err
     writeFile('games.db', data, (err) => {
         if (err) throw err
