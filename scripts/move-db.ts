633be39b14cf784a3586fd4e4debf424276c2694
Kevin Batdorf <kbat82@gmail.com>
2022-05-22 17:46:41 -0400

    wip

--- scripts/move-db.ts
+++ scripts/move-db.ts
@@ -3,7 +3,8 @@ import { join } from 'path'
 
 // move db file
 const oldDb = join(__dirname, '../prisma/games.db')
-readdir(__dirname, (err, files) => {
+console.log({ oldDb })
+readdir(join(__dirname, '../'), (err, files) => {
     files.forEach((file) => {
         console.log(file)
     })
