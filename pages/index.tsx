7447d9a3b90059427087e242d55baa0e59abd877
Kevin Batdorf <kbat82@gmail.com>
2022-05-22 20:45:40 -0400

    Update workflow os

---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+const Page = () => {
+    return (
+        <a href="https://github.com/KevinBatdorf/steam-api">
+            https://github.com/KevinBatdorf/steam-api
+        </a>
+    )
+}
+export default Page
